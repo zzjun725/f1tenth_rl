490fb7d5f1aff1dddfde08ffe371dd4703c37f15
bowen-upenn <90227384+bowen-upenn@users.noreply.github.com>
2022-04-17 21:48:02 -0400

    Add PPO continuous

--- examples/RL_example/f110_rlenv.py
+++ examples/RL_example/f110_rlenv.py
@@ -20,16 +20,23 @@ from math import pi
 
 
 def create_f110env(no_terminal=False, env_time_limit=100000, env_action_repeat=1,
-                   mapfile='./config_example_map.yaml', render=False):
+                   mapfile='./config_example_map.yaml', render=False, continuous_action=False):
     with open(mapfile) as file:
         conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
     limited_time = True if env_time_limit != 0 else False
-    env = F110Env_Discrete_Action(conf=conf,
-                                  no_terminal=no_terminal,
-                                  time_limit=env_time_limit,
-                                  dictObs=True,
-                                  limited_time=limited_time)
+    if continuous_action:
+        env = F110Env_Continuous_Action(conf=conf,
+                                        no_terminal=no_terminal,
+                                        time_limit=env_time_limit,
+                                        dictObs=True,
+                                        limited_time=limited_time)
+    else:
+        env = F110Env_Discrete_Action(conf=conf,
+                                      no_terminal=no_terminal,
+                                      time_limit=env_time_limit,
+                                      dictObs=True,
+                                      limited_time=limited_time)
     if render:
         env.f110.add_render_callback(render_callback)
     return env
@@ -170,6 +177,126 @@ class F110Env_Discrete_Action:
         self.f110.close()
 
 
+########### Bowen Jiang, Added on Apr 17, 2022
+class F110Env_Continuous_Action:
+    def __init__(self, speed=3, conf=None, no_terminal=None, time_limit=10000,
+                 dictObs=False, limited_time=False):
+        self.f110 = F110Env(map=conf.map_path, map_ext=conf.map_ext, num_agents=1)
+        self.conf = conf
+        self.speed = speed
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,))      # in radians
+        self.observation_space = spaces.Box(low=0, high=1000, shape=(27, 1))
+
+        # load waypoints
+        with open(conf.wpt_path, encoding='utf-8') as f:
+            self.waypoints = np.loadtxt(f, delimiter=';')
+            self.waypoints_xytheta = \
+                np.vstack([self.waypoints[:, 1], self.waypoints[:, 2], self.waypoints[:, 3] + pi / 2]).T
+
+        self.no_terminal = no_terminal
+        self.action_size = 1
+        self.time_limit = time_limit
+        self.dictObs = dictObs
+        self.limited_time = limited_time
+
+        self.episode = []
+
+    def reset(self):
+        starting_idx = random.sample(range(len(self.waypoints_xytheta)), 1)
+        # print(self.waypoints_xytheta[starting_idx])
+        starting_pos = self.waypoints_xytheta[starting_idx]
+        # starting_pos[-1] += 0.5
+        raw_obs, _, _, _ = self.f110.reset(starting_pos)
+        # raw_obs, _, _, _ = self.f110.reset(np.array([[self.conf.sx, self.conf.sy, self.conf.stheta]]))
+        obs = self.get_obs(raw_obs)
+
+        # dict
+        if self.dictObs:
+            obs['action'] = np.array(0.0)
+            obs['reward'] = np.array(0.0)
+            obs['terminal'] = np.array(False)
+            obs['reset'] = np.array(True)
+            self.episode = [obs.copy()]
+
+        # time limit
+        self.step_ = 0
+
+        return obs
+
+    def get_action(self, action: int) -> np.ndarray:
+        steer = np.clip(action, a_min=-1, a_max=1)
+        action = np.array([steer, self.speed])
+        return action.reshape(1, -1)
+
+    def get_obs(self, raw_obs: dict):
+        obs = raw_obs['scans'][0][::40]
+        if self.dictObs:
+            if len(obs.shape) == 1:
+                return {'vecobs': obs}  # Vector env
+            else:
+                return {'image': obs}  # Image env
+        else:
+            return obs
+
+    def get_reward(self, raw_obs: dict, done: bool):
+        scan = raw_obs['scans'][0]
+        if done:
+            return -0.5
+        ##  give penalty for near the wall
+        if min(scan) < 0.4:
+            reward = -0.02  # 0.01 -> -0.01
+        else:
+            reward = 0.01
+        ## scale the reward
+        reward *= 2
+
+        return reward
+
+    def step(self, action):
+        action = self.get_action(action)
+        # print(action)
+        # done = False
+        raw_obs, reward, done, info = self.f110.step(action)
+        info = {}
+        ##  make 2 step with the same action
+        # step = 3
+        # while not done and step > 0:
+        #     raw_obs, reward, done, info = self.f110.step(action)
+        #     step -= 1
+        ##  give penalty for hitting the wall
+        obs = self.get_obs(raw_obs)
+        reward = self.get_reward(raw_obs, done)
+
+        # time_limit
+        if self.limited_time:
+            self.step_ += 1
+            if self.step_ >= self.time_limit:
+                done = True
+                info['time_limit'] = True
+
+        ## info
+        if self.dictObs:
+            obs['action'] = action
+            obs['reward'] = np.array(reward)
+            obs['terminal'] = np.array(False if self.no_terminal else done)
+            obs['reset'] = np.array(False)
+
+            self.episode.append(obs.copy())
+            if done:
+                episode = {k: np.array([t[k] for t in self.episode]) for k in self.episode[0]}
+                info['episode'] = episode
+
+        return obs, reward, done, info
+        # return obs, reward, done, info
+
+    def render(self, mode='human'):
+        self.f110.render(mode)
+
+    def close(self):
+        self.f110.close()
+###########
+
+
 if __name__ == '__main__':
     # with open('./config_example_map.yaml') as file:
     #     conf_dict = yaml.load(file, Loader=yaml.FullLoader)
@@ -177,7 +304,7 @@ if __name__ == '__main__':
     #
     # env = F110Env_Discrete_Action(conf=conf, dictObs=True)
     # env.f110.add_render_callback(render_callback)
-    env = create_f110env(no_terminal=False, env_time_limit=10)
+    env = create_f110env(no_terminal=False, env_time_limit=10, continuous_action=False)
 
     for ep_i in range(3):
         obs = env.reset()
--- examples/RL_example/train_f1tenth_ppo.py
+++ examples/RL_example/train_f1tenth_ppo.py
@@ -4,13 +4,19 @@ import os
 import yaml
 from argparse import Namespace
 from utils import render_callback, path_filler
-from f110_rlenv import F110Env_Discrete_Action
+from f110_rlenv import F110Env_Discrete_Action, F110Env_Continuous_Action
 from baselineAgents.ppo_agent import PPOAgent
 
 with open('./config_example_map.yaml') as file:
     conf_dict = yaml.load(file, Loader=yaml.FullLoader)
 conf = Namespace(**conf_dict)
-env = F110Env_Discrete_Action(conf=conf)
+
+continuous_action = True
+if continuous_action:
+    env = F110Env_Continuous_Action(conf=conf)
+else:
+    env = F110Env_Discrete_Action(conf=conf)
+print('continuous_action', continuous_action)
 env.f110.add_render_callback(render_callback)
 
 # train
@@ -21,7 +27,7 @@ os.makedirs(cfg.log_dir, exist_ok=True)
 os.makedirs(cfg.model_dir, exist_ok=True)
 
 model = PPOAgent(env=env, lr_actor=0.001, lr_critic=0.001, gamma=0.99, K_epochs=3,
-                 eps_clip=0.2, conf=cfg)
+                 eps_clip=0.2, conf=cfg, continuous_action=continuous_action)
 model.train(episode_num=10000, update_interval=100, render_interval=5000, save_interval=3000)
 
 
